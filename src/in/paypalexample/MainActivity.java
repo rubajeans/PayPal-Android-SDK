cd94ef52121d6cf1713839b9728e1c8925f43db6
rubajeans <rubajeans@yahoo.fr>
2015-02-12 18:29:08 +0200

    PayPal android sdk
    from form send amount to paypal account

---
+++ src/in/paypalexample/MainActivity.java
@@ -0,0 +1,177 @@
+package in.paypalexample;
+
+import com.paypal.android.sdk.payments.PayPalAuthorization;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalFuturePaymentActivity;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.math.BigDecimal;
+
+
+public class MainActivity extends Activity{
+	 
+	public String amount;
+	private static final String TAG = "paymentExample";
+    /**
+     * - Set to PaymentActivity.ENVIRONMENT_PRODUCTION to move real money.
+     * 
+     * - Set to PaymentActivity.ENVIRONMENT_SANDBOX to use your test credentials
+     * from https://developer.paypal.com
+     * 
+     * - Set to PayPalConfiguration.ENVIRONMENT_NO_NETWORK to kick the tires
+     * without communicating to PayPal's servers.
+     */
+    //private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_NO_NETWORK;
+	private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_SANDBOX;
+
+    // note that these credentials will differ between live & sandbox environments.
+    private static final String CONFIG_CLIENT_ID = "AbtszaScT6xjNtuNIqU-BUJWkj_Q_r-e3_6G3Qtl4FAAWNOpmRvbgkCWbvUCuS7O8HUltt9roB0h96UA";
+
+    private static final int REQUEST_CODE_PAYMENT = 1;
+    private static final int REQUEST_CODE_FUTURE_PAYMENT = 2;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(CONFIG_ENVIRONMENT)
+            .clientId(CONFIG_CLIENT_ID)
+            // The following are only used in PayPalFuturePaymentActivity.
+            .merchantName("Hipster Store")
+            .merchantPrivacyPolicyUri(Uri.parse("https://www.example.com/privacy"))
+            .merchantUserAgreementUri(Uri.parse("https://www.example.com/legal"));
+    
+    PayPalPayment thingToBuy;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+
+		Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(intent);
+		
+	}
+	
+	
+	public void onBuyPressed(View pressed) {
+        // PAYMENT_INTENT_SALE will cause the payment to complete immediately.
+        // Change PAYMENT_INTENT_SALE to PAYMENT_INTENT_AUTHORIZE to only authorize payment and 
+        // capture funds later.
+		EditText txt =(EditText) findViewById(R.id.editText2);
+		amount=txt.getText().toString();
+		char[] money = amount.toCharArray();
+		   
+		if(pressed.getId() == R.id.button1){
+			thingToBuy = new PayPalPayment(new BigDecimal(money), "USD", "Service 1", PayPalPayment.PAYMENT_INTENT_SALE);
+		}
+        
+
+        Intent intent = new Intent(MainActivity.this, PaymentActivity.class);
+
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, thingToBuy);
+
+        startActivityForResult(intent, REQUEST_CODE_PAYMENT);
+    }
+
+    public void onFuturePaymentPressed(View pressed) {
+        Intent intent = new Intent(MainActivity.this, PayPalFuturePaymentActivity.class);
+
+        startActivityForResult(intent, REQUEST_CODE_FUTURE_PAYMENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                PaymentConfirmation confirm =
+                        data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirm != null) {
+                    try {
+                        Log.i(TAG, confirm.toJSONObject().toString(4));
+                        Log.i(TAG, confirm.getPayment().toJSONObject().toString(4));
+
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "PaymentConfirmation info received from PayPal", Toast.LENGTH_LONG)
+                                .show();
+
+                    } catch (JSONException e) {
+                        Log.e(TAG, "an extremely unlikely failure occurred: ", e);
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.i(TAG, "The user canceled.");
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                Log.i(TAG,"An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+            }
+        } else if (requestCode == REQUEST_CODE_FUTURE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                PayPalAuthorization auth =
+                        data.getParcelableExtra(PayPalFuturePaymentActivity.EXTRA_RESULT_AUTHORIZATION);
+                if (auth != null) {
+                    try {
+                        Log.i("FuturePaymentExample", auth.toJSONObject().toString(4));
+
+                        String authorization_code = auth.getAuthorizationCode();
+                        Log.i("FuturePaymentExample", authorization_code);
+
+                        sendAuthorizationToServer(auth);
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Future Payment code received from PayPal", Toast.LENGTH_LONG)
+                                .show();
+
+                    } catch (JSONException e) {
+                        Log.e("FuturePaymentExample", "an extremely unlikely failure occurred: ", e);
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.i("FuturePaymentExample", "The user canceled.");
+            } else if (resultCode == PayPalFuturePaymentActivity.RESULT_EXTRAS_INVALID) {
+                Log.i(
+                        "FuturePaymentExample",
+                        "Probably the attempt to previously start the PayPalService had an invalid PayPalConfiguration. Please see the docs.");
+            }
+        }
+    }
+
+    private void sendAuthorizationToServer(PayPalAuthorization authorization) {
+
+    }
+
+    public void onFuturePaymentPurchasePressed(View pressed) {
+        // Get the Application Correlation ID from the SDK
+        String correlationId = PayPalConfiguration.getApplicationCorrelationId(this);
+
+        Log.i("FuturePaymentExample", "Application Correlation ID: " + correlationId);
+
+        // TODO: Send correlationId and transaction details to your server for processing with
+        // PayPal...
+        Toast.makeText(
+                getApplicationContext(), "App Correlation ID received from SDK", Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    public void onDestroy() {
+        // Stop service when done
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+}
+
